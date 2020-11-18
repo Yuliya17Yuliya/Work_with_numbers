ddcbdfba99f912ed2e5e9aade7ad20a188db9570
Yulia <yulia.malyutina@gmail.com>
2020-11-19 00:30:27 +0300

    Work_with_numbers

---
+++ Work_with_numbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Work_with_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, max = 0;
+            // int idx = 0;
+
+            Console.WriteLine("Введите обрабатываемое число:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int[] A = new int[17];
+
+            for (int i=0; i<3; i++)
+            {
+                x = n % 10;
+                Console.WriteLine($"Цифра {x}");
+                A[i]++;
+
+                n = n / 10;
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 4; i++)
+            {
+                // Console.WriteLine(A[i]);
+
+                if (max < x)
+                {
+                    max = x;
+                    
+                }
+
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Часто поворяемая цифра: {max}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
